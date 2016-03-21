a2ada81cd3ebcd90a5f17a4fcf8fc77752d6aa34
Michael Laccetti <michael@laccetti.com>
2016-03-21 15:01:49 -0400

    Getting the integration tests up and running again

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-jetcd: Java binding for etcd
-============================
+## jetcd: Java binding for etcd
 
 TravisCI: [![Build Status](https://travis-ci.org/mlaccetti/jetcd.png?branch=master)](https://travis-ci.org/mlaccetti/jetcd)
 
@@ -8,7 +7,7 @@ CircleCI: ![CircleCI Status](https://circleci.com/gh/mlaccetti/jetcd.png?circle-
 
 A simple Java client library for the awesome [etcd]
 
-Uses the Apache [HttpAsyncClient] to implement watches without blocking a thread, and Google's [Guava] to give us the nice [ListenableFuture] interface. 
+Uses the Apache [HttpAsyncClient] to implement watches without blocking a thread, and Google's [Guava] to give us the nice [ListenableFuture] interface.
 
 Check out [SmokeTest.java] to see how this is used (and tested), but here's a quick code example:
 
@@ -22,7 +21,7 @@ Assert.assertEquals("hello", result.value);
 
 result = this.client.get(key);
 Assert.assertEquals("hello", result.value);
-        
+
 ListenableFuture<EtcdResult> watchFuture = this.client.watch(key, result.index + 1);
 Assert.assertFalse(watchFuture.isDone());
 
@@ -33,8 +32,28 @@ EtcdResult watchResult = watchFuture.get(100, TimeUnit.MILLISECONDS);
 Assert.assertNotNull(result);
 Assert.assertEquals("world", result.value);
 ```
- 
-For a bit of background, check out the [blog post]
+
+For a bit of background, check out the [blog post].
+
+### Testing
+
+1. Export the IP for the node to use (this example is the typical IP of a `docker-machine` host:
+    ```
+    export HOST_IP="192.168.99.100"
+    ```
+1. Fire up an `etcd` Docker container:
+
+    ```
+  docker run -d -p 4001:4001 -p 2380:2380 -p 2379:2379 --name etcd quay.io/coreos/etcd:v2.0.3 \
+   -name etcd0 \
+   -advertise-client-urls http://${HOST_IP}:2379,http://${HOST_IP}:4001 \
+   -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+   -initial-advertise-peer-urls http://${HOST_IP}:2380 \
+   -listen-peer-urls http://0.0.0.0:2380 \
+   -initial-cluster-token etcd-cluster-1 \
+   -initial-cluster etcd0=http://${HOST_IP}:2380 \
+   -initial-cluster-state new
+    ```
 
 
 [blog post]: http://blog.justinsb.com
--- src/test/java/com/justinsb/etcd/JetcdIntegrationTest.java
+++ src/test/java/com/justinsb/etcd/JetcdIntegrationTest.java
@@ -11,34 +11,34 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import com.justinsb.etcd.EtcdClient;
-import com.justinsb.etcd.EtcdClientException;
-import com.justinsb.etcd.EtcdResult;
 
-public class EtcdIT {
-  String prefix;
-  EtcdClient client;
+@SuppressWarnings("Duplicates")
+public class JetcdIntegrationTest {
+  private String prefix;
+  private EtcdClient client;
 
   @Before
   public void initialize() {
+    final String host = System.getProperty("etcdHost");
+
     this.prefix = "/unittest-" + UUID.randomUUID().toString();
-    this.client = new EtcdClient(URI.create("http://127.0.0.1:4001/"));
+    this.client = new EtcdClient(URI.create(host == null || host.isEmpty() ? "http://127.0.0.1:4001/" : host));
   }
 
   @Test
   public void testEtcdOffline() throws URISyntaxException {
-    EtcdClient client = new EtcdClient(new URI("http://unknowhost"));
+    final EtcdClient client = new EtcdClient(new URI("http://unknowhost"));
     try {
       client.get("key");
       Assert.fail();
-    } catch (EtcdClientException e) {
+    } catch (final EtcdClientException ece) {
       // what now?
     }
   }
 
   @Test
   public void setAndGet() throws Exception {
-    String key = prefix + "/message";
+    final String key = prefix + "/message";
 
     EtcdResult result;
 
@@ -66,9 +66,9 @@ public class EtcdIT {
 
   @Test
   public void getNonExistentKey() throws Exception {
-    String key = prefix + "/doesnotexist";
+    final String key = prefix + "/doesnotexist";
 
-    EtcdResult result;
+    final EtcdResult result;
 
     result = this.client.get(key);
     Assert.assertNull(result);
@@ -76,11 +76,11 @@ public class EtcdIT {
 
   @Test
   public void testDelete() throws Exception {
-    String key = prefix + "/testDelete";
+    final String key = prefix + "/testDelete";
 
     EtcdResult result;
 
-    result = this.client.set(key, "hello");
+    this.client.set(key, "hello");
 
     result = this.client.get(key);
     Assert.assertEquals("hello", result.node.value);
@@ -96,20 +96,19 @@ public class EtcdIT {
 
   @Test
   public void deleteNonExistentKey() throws Exception {
-    String key = prefix + "/doesnotexist";
+    final String key = prefix + "/doesnotexist";
 
     try {
-      /* EtcdResult result = */
       this.client.delete(key);
       Assert.fail();
-    } catch (EtcdClientException e) {
+    } catch (final EtcdClientException e) {
       Assert.assertTrue(e.isEtcdError(100));
     }
   }
 
   @Test
   public void testTtl() throws Exception {
-    String key = prefix + "/ttl";
+    final String key = prefix + "/ttl";
 
     EtcdResult result;
 
@@ -129,11 +128,11 @@ public class EtcdIT {
 
   @Test
   public void testCAS() throws Exception {
-    String key = prefix + "/cas";
+    final String key = prefix + "/cas";
 
     EtcdResult result;
 
-    result = this.client.set(key, "hello");
+    this.client.set(key, "hello");
     result = this.client.get(key);
     Assert.assertEquals("hello", result.node.value);
 
@@ -150,19 +149,19 @@ public class EtcdIT {
 
   @Test
   public void testWatchPrefix() throws Exception {
-    String key = prefix + "/watch";
+    final String key = prefix + "/watch";
 
     EtcdResult result = this.client.set(key + "/f2", "f2");
     Assert.assertTrue(!result.isError());
     Assert.assertNotNull(result.node);
     Assert.assertEquals("f2", result.node.value);
 
-    ListenableFuture<EtcdResult> watchFuture = this.client.watch(key,
+    final ListenableFuture<EtcdResult> watchFuture = this.client.watch(key,
         result.node.modifiedIndex + 1, true);
     try {
-      EtcdResult watchResult = watchFuture.get(100, TimeUnit.MILLISECONDS);
+      final EtcdResult watchResult = watchFuture.get(100, TimeUnit.MILLISECONDS);
       Assert.fail("Subtree watch fired unexpectedly: " + watchResult);
-    } catch (TimeoutException e) {
+    } catch (final TimeoutException e) {
       // Expected
     }
 
@@ -173,7 +172,7 @@ public class EtcdIT {
     Assert.assertNotNull(result.node);
     Assert.assertEquals("f1", result.node.value);
 
-    EtcdResult watchResult = watchFuture.get(100, TimeUnit.MILLISECONDS);
+    final EtcdResult watchResult = watchFuture.get(100, TimeUnit.MILLISECONDS);
 
     Assert.assertNotNull(watchResult);
     Assert.assertTrue(!watchResult.isError());
@@ -191,7 +190,7 @@ public class EtcdIT {
 
   @Test
   public void testList() throws Exception {
-    String key = prefix + "/dir";
+    final String key = prefix + "/dir";
 
     EtcdResult result;
 
@@ -204,30 +203,30 @@ public class EtcdIT {
     result = this.client.set(key + "/subdir1/f", "f");
     Assert.assertEquals("f", result.node.value);
 
-    EtcdResult listing = this.client.listChildren(key);
+    final EtcdResult listing = this.client.listChildren(key);
     Assert.assertEquals(4, listing.node.nodes.size());
     Assert.assertEquals("get", listing.action);
 
     {
-      EtcdNode child = listing.node.nodes.get(0);
+      final EtcdNode child = listing.node.nodes.get(0);
       Assert.assertEquals(key + "/f1", child.key);
       Assert.assertEquals("f1", child.value);
       Assert.assertEquals(false, child.dir);
     }
     {
-      EtcdNode child = listing.node.nodes.get(1);
+      final EtcdNode child = listing.node.nodes.get(1);
       Assert.assertEquals(key + "/f2", child.key);
       Assert.assertEquals("f2", child.value);
       Assert.assertEquals(false, child.dir);
     }
     {
-      EtcdNode child = listing.node.nodes.get(2);
+      final EtcdNode child = listing.node.nodes.get(2);
       Assert.assertEquals(key + "/f3", child.key);
       Assert.assertEquals("f3", child.value);
       Assert.assertEquals(false, child.dir);
     }
     {
-      EtcdNode child = listing.node.nodes.get(3);
+      final EtcdNode child = listing.node.nodes.get(3);
       Assert.assertEquals(key + "/subdir1", child.key);
       Assert.assertEquals(null, child.value);
       Assert.assertEquals(true, child.dir);
@@ -236,7 +235,7 @@ public class EtcdIT {
 
   @Test
   public void testGetVersion() throws Exception {
-    String version = this.client.getVersion();
-    Assert.assertTrue(version.startsWith("etcd 0."));
+    final String version = this.client.getVersion();
+    Assert.assertTrue("Expected version 'etcd 2.0...', but got " + version, version.startsWith("{\"releaseVersion\":\"2.0"));
   }
 }
